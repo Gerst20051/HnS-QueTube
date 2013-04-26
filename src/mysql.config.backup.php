ed7a39f32d5cc7743c5da768fdde12c330fb7d82
Andrew Gerst <gerst20051@gmail.com>
2013-04-26 18:29:52 -0400

    updated to git files

---
+++ src/mysql.config.backup.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'config.inc.php';
+
+if (LOCAL) {
+	define('MYSQL_HOST','');
+	define('MYSQL_USER','');
+	define('MYSQL_PASSWORD','');
+	define('MYSQL_DATABASE','');
+	define('MYSQL_TABLE','');
+} else {
+	define('MYSQL_HOST','');
+	define('MYSQL_USER','');
+	define('MYSQL_PASSWORD','');
+	define('MYSQL_DATABASE','');
+	define('MYSQL_TABLE','');
+}
+?>
